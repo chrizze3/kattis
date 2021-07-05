0393368c700cea402799d25998392306758a21ae
unknown <chrth679@student.liu.se>
2021-07-05 18:58:59 +0200

    filip exercise added

---
+++ filip/java/src/Filip.java
@@ -0,0 +1,11 @@
+public class Filip {
+    public static void main(String args[]) {
+        Kattio io = new Kattio(System.in, System.out);
+        while (io.hasMoreTokens()) {
+            
+        }
+
+        io.print("Hello World!");
+        io.close();
+    }
+}
