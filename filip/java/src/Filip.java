993e32b58db9eed96ee8dbf812c1584d2ab16228
unknown <chrth679@student.liu.se>
2021-07-05 19:45:37 +0200

    Filip: another solution, probably not any better

--- filip/java/src/Filip.java
+++ filip/java/src/Filip.java
@@ -7,7 +7,7 @@ public class Filip {
     public static Kattio io;
     public static void kattioInputFromFile() {
         try {
-            io = new Kattio(new FileInputStream(new File("src/filip.dummy.1.in")), System.out);
+            io = new Kattio(new FileInputStream(new File("src/filip.dummy.3.in")), System.out);
         } catch (IOException e) {
             System.out.println(e);
         }
@@ -28,29 +28,31 @@ public class Filip {
         int m = io.getInt();
         int n_last_digit = n % 10;
         int m_last_digit = m % 10;
-        if (n_last_digit > m_last_digit) {
-            printNumberReverse(n);
-        } else if (n_last_digit < m_last_digit) {
-            printNumberReverse(m);
-        } else {
-            int n_original = n;
-            int m_original = m;
+        if (n_last_digit == m_last_digit) {
+            io.print(n_last_digit);
             n /= 10;
             m /= 10;
             n_last_digit = n % 10;
             m_last_digit = m % 10;
-            if (n_last_digit > m_last_digit) {
-                printNumberReverse(n_original);
-            } else if (n_last_digit < m_last_digit) {
-                printNumberReverse(m_original);
-            } else {
+            if (n_last_digit == m_last_digit) {
+                io.print(n_last_digit);
                 n /= 10;
                 m /= 10;
-                if (n > m) {
+                n_last_digit = n % 10;
+                m_last_digit = m % 10;
+                io.print(Math.max(n_last_digit, m_last_digit));
+            } else {
+                if (n_last_digit > m_last_digit) {
                     printNumberReverse(n);
                 } else {
                     printNumberReverse(m);
                 }
+            }
+        } else {
+            if (n_last_digit > m_last_digit) {
+                printNumberReverse(n);
+            } else {
+                printNumberReverse(m);
             }
         }
         io.close();
